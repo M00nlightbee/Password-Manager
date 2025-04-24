ab8a132d00fea5e898d49a593c062410f6542d55
M00nlightbee <149159857+M00nlightbee@users.noreply.github.com>
2025-04-24 03:36:58 +0100

    Add project files.

---
+++ Assessment/Main.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace Assessment
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+			topBoard = new Panel();
+			lbTitle = new Label();
+			logo = new PictureBox();
+			logoName = new Label();
+			swOne = new PictureBox();
+			swTwo = new PictureBox();
+			swThree = new PictureBox();
+			swFour = new PictureBox();
+			lbLower = new Label();
+			lbUpper = new Label();
+			lbSymbol = new Label();
+			lbNumber = new Label();
+			plLabel = new Label();
+			llValue = new TextBox();
+			btnPassword = new Button();
+			ulValue = new TextBox();
+			scValue = new TextBox();
+			numValue = new TextBox();
+			plHolder = new Panel();
+			plValue = new Label();
+			pwDisplay = new Label();
+			pwHolder = new Panel();
+			topBoard.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)logo).BeginInit();
+			((System.ComponentModel.ISupportInitialize)swOne).BeginInit();
+			((System.ComponentModel.ISupportInitialize)swTwo).BeginInit();
+			((System.ComponentModel.ISupportInitialize)swThree).BeginInit();
+			((System.ComponentModel.ISupportInitialize)swFour).BeginInit();
+			plHolder.SuspendLayout();
+			pwHolder.SuspendLayout();
+			SuspendLayout();
+			// 
+			// topBoard
+			// 
+			topBoard.BackColor = Color.Black;
+			topBoard.Controls.Add(lbTitle);
+			topBoard.Controls.Add(logo);
+			topBoard.Controls.Add(logoName);
+			topBoard.Location = new Point(0, 0);
+			topBoard.Name = "topBoard";
+			topBoard.Size = new Size(965, 141);
+			topBoard.TabIndex = 0;
+			// 
+			// lbTitle
+			// 
+			lbTitle.AutoSize = true;
+			lbTitle.Font = new Font("Sitka Heading", 18F, FontStyle.Regular, GraphicsUnit.Point);
+			lbTitle.ForeColor = Color.White;
+			lbTitle.Location = new Point(290, 95);
+			lbTitle.Name = "lbTitle";
+			lbTitle.Size = new Size(427, 43);
+			lbTitle.TabIndex = 12;
+			lbTitle.Text = "Welcome to Password Generator";
+			// 
+			// logo
+			// 
+			logo.BackgroundImage = (Image)resources.GetObject("logo.BackgroundImage");
+			logo.BackgroundImageLayout = ImageLayout.None;
+			logo.Location = new Point(24, 45);
+			logo.Name = "logo";
+			logo.Size = new Size(51, 51);
+			logo.TabIndex = 1;
+			logo.TabStop = false;
+			// 
+			// logoName
+			// 
+			logoName.AutoSize = true;
+			logoName.Font = new Font("Segoe UI Historic", 12F, FontStyle.Regular, GraphicsUnit.Point);
+			logoName.ForeColor = Color.White;
+			logoName.Location = new Point(81, 57);
+			logoName.Name = "logoName";
+			logoName.Size = new Size(187, 28);
+			logoName.TabIndex = 1;
+			logoName.Text = "Password Generator";
+			// 
+			// swOne
+			// 
+			swOne.BackgroundImage = Properties.Resources.SwitchOff;
+			swOne.BackgroundImageLayout = ImageLayout.None;
+			swOne.Location = new Point(623, 212);
+			swOne.Name = "swOne";
+			swOne.Size = new Size(57, 37);
+			swOne.TabIndex = 2;
+			swOne.TabStop = false;
+			swOne.MouseClick += swOne_MouseClick;
+			// 
+			// swTwo
+			// 
+			swTwo.BackgroundImage = Properties.Resources.SwitchOff;
+			swTwo.BackgroundImageLayout = ImageLayout.None;
+			swTwo.Location = new Point(623, 255);
+			swTwo.Name = "swTwo";
+			swTwo.Size = new Size(57, 37);
+			swTwo.TabIndex = 4;
+			swTwo.TabStop = false;
+			swTwo.MouseClick += swTwo_MouseClick;
+			// 
+			// swThree
+			// 
+			swThree.BackgroundImage = Properties.Resources.SwitchOff;
+			swThree.BackgroundImageLayout = ImageLayout.None;
+			swThree.Location = new Point(623, 297);
+			swThree.Name = "swThree";
+			swThree.Size = new Size(57, 37);
+			swThree.TabIndex = 5;
+			swThree.TabStop = false;
+			swThree.MouseClick += swThree_MouseClick;
+			// 
+			// swFour
+			// 
+			swFour.BackgroundImage = Properties.Resources.SwitchOff;
+			swFour.BackgroundImageLayout = ImageLayout.None;
+			swFour.Location = new Point(623, 339);
+			swFour.Name = "swFour";
+			swFour.Size = new Size(57, 37);
+			swFour.TabIndex = 6;
+			swFour.TabStop = false;
+			swFour.MouseClick += swFour_MouseClick;
+			// 
+			// lbLower
+			// 
+			lbLower.AutoSize = true;
+			lbLower.Font = new Font("Segoe UI Historic", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+			lbLower.Location = new Point(287, 212);
+			lbLower.Name = "lbLower";
+			lbLower.Size = new Size(150, 25);
+			lbLower.TabIndex = 8;
+			lbLower.Text = "Lowercase Letters";
+			// 
+			// lbUpper
+			// 
+			lbUpper.AutoSize = true;
+			lbUpper.Font = new Font("Segoe UI Historic", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+			lbUpper.Location = new Point(287, 255);
+			lbUpper.Name = "lbUpper";
+			lbUpper.Size = new Size(152, 25);
+			lbUpper.TabIndex = 9;
+			lbUpper.Text = "Uppercase Letters";
+			// 
+			// lbSymbol
+			// 
+			lbSymbol.AutoSize = true;
+			lbSymbol.Font = new Font("Segoe UI Historic", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+			lbSymbol.Location = new Point(287, 297);
+			lbSymbol.Name = "lbSymbol";
+			lbSymbol.Size = new Size(222, 25);
+			lbSymbol.TabIndex = 10;
+			lbSymbol.Text = "Special Character/Symbols";
+			// 
+			// lbNumber
+			// 
+			lbNumber.AutoSize = true;
+			lbNumber.Font = new Font("Segoe UI Historic", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+			lbNumber.Location = new Point(287, 339);
+			lbNumber.Name = "lbNumber";
+			lbNumber.Size = new Size(85, 25);
+			lbNumber.TabIndex = 11;
+			lbNumber.Text = "Numbers";
+			// 
+			// plLabel
+			// 
+			plLabel.AutoSize = true;
+			plLabel.Font = new Font("Segoe UI Historic", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+			plLabel.Location = new Point(289, 413);
+			plLabel.Name = "plLabel";
+			plLabel.Size = new Size(147, 25);
+			plLabel.TabIndex = 12;
+			plLabel.Text = "Password Length";
+			// 
+			// llValue
+			// 
+			llValue.Location = new Point(693, 213);
+			llValue.Name = "llValue";
+			llValue.Size = new Size(46, 27);
+			llValue.TabIndex = 13;
+			llValue.TextAlign = HorizontalAlignment.Center;
+			// 
+			// btnPassword
+			// 
+			btnPassword.BackColor = Color.Lime;
+			btnPassword.FlatStyle = FlatStyle.Popup;
+			btnPassword.Font = new Font("Segoe UI Historic", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+			btnPassword.ForeColor = Color.White;
+			btnPassword.Location = new Point(442, 585);
+			btnPassword.Name = "btnPassword";
+			btnPassword.Size = new Size(197, 53);
+			btnPassword.TabIndex = 16;
+			btnPassword.Text = "Generate Password";
+			btnPassword.UseVisualStyleBackColor = false;
+			btnPassword.Click += btnPassword_Click;
+			// 
+			// ulValue
+			// 
+			ulValue.Location = new Point(693, 256);
+			ulValue.Name = "ulValue";
+			ulValue.Size = new Size(46, 27);
+			ulValue.TabIndex = 17;
+			ulValue.TextAlign = HorizontalAlignment.Center;
+			// 
+			// scValue
+			// 
+			scValue.Location = new Point(693, 299);
+			scValue.Name = "scValue";
+			scValue.Size = new Size(46, 27);
+			scValue.TabIndex = 18;
+			scValue.TextAlign = HorizontalAlignment.Center;
+			// 
+			// numValue
+			// 
+			numValue.Location = new Point(693, 339);
+			numValue.Name = "numValue";
+			numValue.Size = new Size(46, 27);
+			numValue.TabIndex = 19;
+			numValue.TextAlign = HorizontalAlignment.Center;
+			// 
+			// plHolder
+			// 
+			plHolder.BackColor = Color.Black;
+			plHolder.Controls.Add(plValue);
+			plHolder.ForeColor = SystemColors.Window;
+			plHolder.Location = new Point(442, 409);
+			plHolder.Name = "plHolder";
+			plHolder.Size = new Size(67, 29);
+			plHolder.TabIndex = 23;
+			// 
+			// plValue
+			// 
+			plValue.AutoSize = true;
+			plValue.ForeColor = SystemColors.ButtonHighlight;
+			plValue.Location = new Point(11, 5);
+			plValue.Name = "plValue";
+			plValue.Size = new Size(0, 20);
+			plValue.TabIndex = 0;
+			plValue.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// pwDisplay
+			// 
+			pwDisplay.AutoSize = true;
+			pwDisplay.BackColor = Color.Transparent;
+			pwDisplay.Font = new Font("Arial Narrow", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+			pwDisplay.ForeColor = SystemColors.ControlLightLight;
+			pwDisplay.Location = new Point(29, 16);
+			pwDisplay.Name = "pwDisplay";
+			pwDisplay.Size = new Size(0, 27);
+			pwDisplay.TabIndex = 21;
+			pwDisplay.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// pwHolder
+			// 
+			pwHolder.BackColor = Color.Black;
+			pwHolder.Controls.Add(pwDisplay);
+			pwHolder.ForeColor = Color.Coral;
+			pwHolder.Location = new Point(349, 497);
+			pwHolder.Name = "pwHolder";
+			pwHolder.Size = new Size(405, 59);
+			pwHolder.TabIndex = 22;
+			pwHolder.Tag = "";
+			// 
+			// frmMain
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			BackColor = SystemColors.ButtonHighlight;
+			ClientSize = new Size(962, 673);
+			Controls.Add(plHolder);
+			Controls.Add(pwHolder);
+			Controls.Add(numValue);
+			Controls.Add(scValue);
+			Controls.Add(ulValue);
+			Controls.Add(btnPassword);
+			Controls.Add(llValue);
+			Controls.Add(plLabel);
+			Controls.Add(lbNumber);
+			Controls.Add(lbSymbol);
+			Controls.Add(lbUpper);
+			Controls.Add(lbLower);
+			Controls.Add(swFour);
+			Controls.Add(swThree);
+			Controls.Add(swTwo);
+			Controls.Add(swOne);
+			Controls.Add(topBoard);
+			MaximizeBox = false;
+			Name = "frmMain";
+			Text = "Main";
+			topBoard.ResumeLayout(false);
+			topBoard.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)logo).EndInit();
+			((System.ComponentModel.ISupportInitialize)swOne).EndInit();
+			((System.ComponentModel.ISupportInitialize)swTwo).EndInit();
+			((System.ComponentModel.ISupportInitialize)swThree).EndInit();
+			((System.ComponentModel.ISupportInitialize)swFour).EndInit();
+			plHolder.ResumeLayout(false);
+			plHolder.PerformLayout();
+			pwHolder.ResumeLayout(false);
+			pwHolder.PerformLayout();
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Panel topBoard;
+        private Label logoName;
+        private PictureBox logo;
+        private PictureBox swOne;
+        private PictureBox swTwo;
+        private PictureBox swThree;
+        private PictureBox swFour;
+        private Label lbLower;
+        private Label lbUpper;
+        private Label lbSymbol;
+        private Label lbNumber;
+        private Label lbTitle;
+        private Label plLabel;
+        private TextBox llValue;
+        private Button btnPassword;
+        private TextBox ulValue;
+        private TextBox scValue;
+        private TextBox numValue;
+        private Panel plHolder;
+        private Label plValue;
+		private Label pwDisplay;
+		private Panel pwHolder;
+	}
+}
---
+++ Assessment/Main.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Microsoft.VisualBasic;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Assessment
+{
+	public partial class frmMain : Form
+	{
+		Image toggleOn;
+		Image toggleOff;
+
+		public frmMain()
+		{
+			InitializeComponent();
+			toggleOn = Image.FromFile("SwitchOn.png");
+			toggleOff = Image.FromFile("SwitchOff.png");
+			llValue.Hide();
+			ulValue.Hide();
+			scValue.Hide();
+			numValue.Hide();
+
+
+		}
+
+		//target pictureBox to toggle (https://stackoverflow.com/questions/13412605/get-picturebox-name)
+		//public void toggle(object sender, EventArgs e)
+		//{
+		//    Character c = new Character();
+		//    ArrayList password = new ArrayList();
+		//    foreach (var item in $"{c.lowerCase}{c.upperCase}{c.number}{c.symbol}")
+		//    {
+		//        password.Add(item);
+		//    };
+
+		//    if (sender is PictureBox)
+		//    {
+		//        if (((PictureBox)sender).Image != toggleOn)
+		//        {
+		//            ((PictureBox)sender).Image = toggleOn;
+		//        }
+		//        else
+		//        {
+		//            ((PictureBox)sender).Image = toggleOff;
+		//        }
+		//    }
+
+		//}
+
+		private void swOne_MouseClick(object sender, MouseEventArgs e)
+		{
+
+			if (e.Button == MouseButtons.Left)
+			{
+				if (swOne.Image != toggleOn)
+				{
+					swOne.Image = toggleOn;
+					llValue.Show();
+				}
+				else
+				{
+					swOne.Image = toggleOff;
+					llValue.Clear();
+					llValue.Hide();
+				}
+			}
+		}
+
+		private void swTwo_MouseClick(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+			{
+				if (swTwo.Image != toggleOn)
+				{
+					swTwo.Image = toggleOn;
+					ulValue.Show();
+				}
+				else
+				{
+					swTwo.Image = toggleOff;
+					ulValue.Clear();
+					ulValue.Hide();
+				}
+			}
+		}
+
+		private void swThree_MouseClick(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+			{
+				if (swThree.Image != toggleOn)
+				{
+					swThree.Image = toggleOn;
+					scValue.Show();
+				}
+				else
+				{
+					swThree.Image = toggleOff;
+					scValue.Clear();
+					scValue.Hide();
+				}
+			}
+		}
+
+		private void swFour_MouseClick(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+			{
+				if (swFour.Image != toggleOn)
+				{
+					swFour.Image = toggleOn;
+					numValue.Show();
+				}
+				else
+				{
+					swFour.Image = toggleOff;
+					numValue.Clear();
+					numValue.Hide();
+				}
+			}
+		}
+		private void btnPassword_Click(object sender, EventArgs e)
+		{
+			Character c = new Character();
+			ArrayList password = new ArrayList();
+			foreach (char item in $"{c.lowerCase}{c.upperCase}{c.number}{c.symbol}")
+			{
+				password.Add(item);
+			}
+			;
+
+			//ordered password
+			string oPassword = "";
+
+			int llCase;
+			while ((!int.TryParse(llValue.Text, out llCase) || llCase < 0 || llCase > c.lowerCase.Length) && llValue.Text.Length > 0)
+			{
+				MessageBox.Show($"Number of LOWER CASES is INVALID .\nENTER a valid integer between 1 and {c.lowerCase.Length}");
+				break;
+			}
+			int ulCase;
+			while ((!int.TryParse(ulValue.Text, out ulCase) || llCase < 0 || ulCase > c.upperCase.Length) && ulValue.Text.Length > 0)
+			{
+				MessageBox.Show($"Number of UPPER CASES is INVALID.\nENTER a valid integer between 1 and {c.upperCase.Length}");
+				break;
+			}
+			int scCase;
+			while ((!int.TryParse(scValue.Text, out scCase) || scCase < 0 || scCase > c.symbol.Length) && scValue.Text.Length > 0)
+			{
+				MessageBox.Show($"Number of SPECIAL CHARATERS is INVALID.\nENTER a valid integer between 1 and {c.symbol.Length}");
+				break;
+			}
+			int numCase;
+			while ((!int.TryParse(numValue.Text, out numCase) || numCase < 0 || numCase > c.number.Length) && numValue.Text.Length > 0)
+			{
+				MessageBox.Show($"Number of NUMBERS is INVALID.\nENTER a valid integer between 1 and {c.number.Length}");
+				break;
+			}
+
+			//for (int i = 0; i < llCase - 1; i++)
+			//{
+			//	if (llCase < c.lowerCase.Length)
+			//	{
+			//		oPassword += $"{c.lowerCase[i]}";
+			//		password.RemoveAt(i);
+			//	}
+			//}
+			//for (int i = 0; i < ulCase - 1; i++)
+			//{
+			//	if (llCase < c.upperCase.Length)
+			//	{
+			//		oPassword += $"{c.upperCase[i]}";
+			//		password.RemoveAt(i);
+			//	}
+			//}
+			//for (int i = 0; i < scCase - 1; i++)
+			//{
+			//	if (llCase < c.symbol.Length)
+			//	{
+			//		oPassword += $"{c.symbol[i]}";
+			//		password.RemoveAt(i);
+			//	}
+			//}
+			//for (int i = 0; i < numCase - 1; i++)
+			//{
+			//	if (llCase < c.number.Length)
+			//	{
+			//		oPassword += $"{c.number[i]}";
+			//		password.RemoveAt(i);
+			//	}
+			//}
+
+			//int llCase;
+			//int ulCase;
+			//int scCase;
+			//int numCase;
+
+			//while ((!int.TryParse(llValue.Text, out llCase) || llCase < 0 || llCase > 6) && llValue.Text.Length > 0)
+			//{
+			//	MessageBox.Show($"Number of LOWER CASES is INVALID .\nENTER a valid integer between 1 and 6");
+			//	break;
+			//}
+
+			//while ((!int.TryParse(ulValue.Text, out ulCase) || llCase < 0 || ulCase > 6) && ulValue.Text.Length > 0)
+			//{
+			//	MessageBox.Show($"Number of UPPER CASES is INVALID.\nENTER a valid integer between 1 and 6");
+			//	break;
+			//}
+
+			//while ((!int.TryParse(scValue.Text, out scCase) || scCase < 0 || scCase > 7) && scValue.Text.Length > 0)
+			//{
+			//	MessageBox.Show($"Number of SPECIAL CHARATERS is INVALID.\nENTER a valid integer between 1 and 7");
+			//	break;
+			//}
+
+			//while ((!int.TryParse(numValue.Text, out numCase) || numCase < 0 || numCase > 7) && numValue.Text.Length > 0)
+			//{
+			//	MessageBox.Show($"Number of NUMBERS is INVALID.\nENTER a valid integer between 1 and 7");
+			//	break;
+			//}
+
+			for (int i = 0; i < llCase; i++)
+			{
+				if (llCase < c.lowerCase.Length)
+				{
+					oPassword += $"{c.lowerCase[i]}";
+					password.RemoveAt(i);
+				}
+			}
+			for (int i = 0; i < ulCase; i++)
+			{
+				if (ulCase < c.upperCase.Length)
+				{
+					oPassword += $"{c.upperCase[i]}";
+					password.RemoveAt(i);
+				}
+			}
+			for (int i = 0; i < scCase; i++)
+			{
+				if (scCase < c.symbol.Length)
+				{
+					oPassword += $"{c.symbol[i]}";
+					password.RemoveAt(i);
+				}
+			}
+			for (int i = 0; i < numCase ; i++)
+			{
+				if (numCase < c.number.Length)
+				{
+					oPassword += $"{c.number[i]}";
+					password.RemoveAt(i);
+				}
+			}
+
+			//randomise password
+			ArrayList GeneratedPassword = new ArrayList();
+
+			foreach (char item in $"{oPassword}")
+			{
+				GeneratedPassword.Add(item);
+			}
+
+			string randPassword = "";
+
+			Random r = new Random();
+
+			while (GeneratedPassword.Count > 0)
+			{
+				int index = r.Next(0, GeneratedPassword.Count);
+				randPassword += GeneratedPassword[index];
+				GeneratedPassword.RemoveAt(index);
+			}
+
+			if (randPassword.Length <= 0)
+			{
+				MessageBox.Show("Choose at least 2 from the above to generate password");
+			}
+			else if (randPassword.Length > 26)
+			{
+				MessageBox.Show("Password too long Max Length is 26");
+			}
+			else
+			{
+				pwDisplay.Text = randPassword;
+			}
+
+			//show total length of password
+			plValue.Text = Convert.ToString(llCase + ulCase + scCase + numCase);
+		}
+
+		//private void pwHolder_Paint(object sender, PaintEventArgs e)
+		//{
+
+		//}
+	}
+}
